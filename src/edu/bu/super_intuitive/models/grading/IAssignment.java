0bcad3af0c4b0feb9b486c564d3f3fda9fade908
青椒 <10142130151_ecnu@outlook.com>
2022-05-10 13:59:28 -0400

    进一步添加注释

--- src/edu/bu/super_intuitive/models/grading/IAssignment.java
+++ src/edu/bu/super_intuitive/models/grading/IAssignment.java
@@ -13,15 +13,69 @@ public interface IAssignment {
     int getAssignmentId();
     ICourse getCourse() throws InstantiationException;
 
+    /**
+     * @Author Zhenghang Yin
+     * @return the weight of an assignment.
+     * @throws OperationFailed
+     */
     int getWeight() throws OperationFailed;
-    void setWeight(int weight) throws OperationFailed;
-    String getName() throws OperationFailed;
 
+    /**
+     * Set the score of an assignment for a student.
+     * @param weight the weight of the assignment.
+     * @throws OperationFailed
+     */
+    void setWeight(int weight) throws OperationFailed;
+
+    /**
+     * @Author Zhenghang Yin
+     * @return the name of the assignment.
+     * @throws OperationFailed
+     */
+    String getName() throws OperationFailed;
+    /**
+     * @Author Zhenghang Yin
+     * @return the full score of the assignment.
+     * @throws OperationFailed
+     */
     int getFullScore() throws OperationFailed;
+
+    /**
+     * Set the full score of the assignment.
+     * @Author Zhenghang Yin
+     * @param fullScore the full score of the assignment.
+     * @throws OperationFailed
+     */
     void setFullScore(int fullScore) throws OperationFailed;
 
+    /**
+     * Set the score of an assignment for a student.
+     * @param student the student.
+     * @param score the score of the assignment.
+     * @throws OperationFailed
+     */
     void setStudentScore(IStudent student, int score) throws OperationFailed;
+
+    /**
+     * @Author Zhenghang Yin
+     * @param student the student.
+     * @return the score of the assignment for a student.
+     * @throws OperationFailed
+     */
     int getStudentScore(IStudent student) throws OperationFailed;
+
+    /**
+     * @Author Zhenghang Yin
+     * @param student the student.
+     * @return the score of the assignment for a student.
+     */
     boolean hasStudentScore(IStudent student);
+
+    /**
+     * Remove the score of an assignment for a student.
+     * @Author Zhenghang Yin
+     * @param student the student.
+     * @throws OperationFailed
+     */
     void removeStudentScore(IStudent student) throws OperationFailed;
 }
--- src/edu/bu/super_intuitive/models/grading/IMember.java
+++ src/edu/bu/super_intuitive/models/grading/IMember.java
@@ -10,9 +10,14 @@ package edu.bu.super_intuitive.models.grading;
 import edu.bu.super_intuitive.models.exception.OperationFailed;
 
 public interface IMember {
+    // get the BUID
     String getBUId();
+    // get the name
     String getName();
+    // set the name
     void setName(String name) throws OperationFailed;
+    // get the email
     String getEmail();
+    // set the email
     void setEmail(String email) throws OperationFailed;
 }
