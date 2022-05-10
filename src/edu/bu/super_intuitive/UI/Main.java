08f88ca1e63c4b0875294d089b150de08eb7fd58
Harry <chenhy19980116@gmail.com>
2022-05-10 04:38:03 -0400

    Update add name to course

--- src/edu/bu/super_intuitive/UI/Main.java
+++ src/edu/bu/super_intuitive/UI/Main.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Login();
+
+        var frame = new CourseView2();
+        frame.setVisible(true);
     }
 }
